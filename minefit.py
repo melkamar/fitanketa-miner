8fd2075a2ede9ca791dab1b54739d39fcb65c7db
Martin Melka <melkamar@fit.cvut.cz>
2017-06-17 21:34:27 +0200

    Extract markdown generation methods to a separate class

--- minefit.py
+++ minefit.py
@@ -13,6 +13,153 @@ import publisher
 import util
 
 
+class SiteGenerator:
+    def __init__(self, index_root, courses_root, data, semester):
+        super().__init__()
+        self.index_root = index_root
+        self.courses_root = courses_root
+        self.data = data
+        self.semester = semester
+
+    def generate_page(self):
+        self._make_pages(6)
+        self._make_index()
+
+    def _make_index(self):
+        if not os.path.exists(self.courses_root):
+            raise ValueError(f"Specified root directory does not exist: {os.path.abspath(self.courses_root)}")
+
+        index_md = "# Přehled splněných předmětů\n"
+        programme_semesters_dict = {}
+        for semester in os.listdir(self.courses_root):
+            for programme in os.listdir(os.path.join(self.courses_root, semester)):
+                # print(f"{semester} -> {programme}")
+                if programme not in programme_semesters_dict:
+                    programme_semesters_dict[programme] = []
+
+                programme_semesters_dict[programme].append(semester)
+
+        for programme, semesters in programme_semesters_dict.items():
+            programme_name_no_ext = programme.rsplit(".", 1)[0]
+
+            index_md += f'## Program {programme_name_no_ext}\n'
+            for semester in semesters:
+                index_md += f'- [{util.semester_id_to_str(semester)}]({os.path.join(os.path.relpath(self.courses_root, self.index_root),semester,programme_name_no_ext)})\n'
+
+            index_md += '\n\n'
+
+        with open(os.path.join(self.index_root, 'README.md'), 'w', encoding='utf-8') as f:
+            f.write(index_md)
+
+    def _make_page_heading_index(self, index_columns, courses_data):
+        """Create index of anchor links to given courses."""
+        # {course_name} ({course_id})
+
+        index_matrix = []
+        index_rows = math.ceil(len(courses_data) / index_columns)
+        [index_matrix.append([]) for _ in range(index_rows)]
+
+        sorted_courses_data = sorted(courses_data, key=lambda item: item[0]['course_id'])
+        for i, course_data in enumerate(sorted_courses_data):
+            course_id = course_data[0]['course_id']
+            course_name = course_data[0]['course_name']
+            print(course_id)
+            anchor_link = f'#{course_name.lower()}-{course_id.lower()}'.replace(' ', '-').replace('.', '')
+            anchor_full = f'[{course_id}]({anchor_link})'
+
+            cur_row = i // index_columns
+
+            index_matrix[cur_row].append(anchor_full)
+
+        from pprint import pprint
+        pprint(index_matrix)
+        md_heading = f'{"| "*index_columns}|'
+        md_separator = md_heading.replace(' ', '-')
+        md_content = ''
+
+        for row in index_matrix:
+            md_content += f'|{" | ".join(row)}|\n'
+
+        md_index = f'''
+    {md_heading}
+    {md_separator}
+    {md_content}
+        '''
+        print(md_index)
+        return md_index
+
+    def _make_pages(self, index_columns=6):
+        for semester_programme, semester_courses in self.data.items():
+            programme_md_heading = f"# {util.semester_id_to_str(self.semester)} - předměty programu {semester_programme}"
+            programme_md_tables = ""
+            programme_md_footer = f'\n\n*Stav k {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}*'
+
+            programme_md_heading_index = self._make_page_heading_index(index_columns, semester_courses.values())
+
+            for course_id, course_data in semester_courses.items():
+                md_page = self._make_md_table(semester_programme, course_data)
+                programme_md_tables += md_page
+                programme_md_tables += "\n"
+
+            tgt_dir = os.path.join(self.courses_root, self.semester)
+            os.makedirs(tgt_dir, exist_ok=True)
+            with open(os.path.join(tgt_dir, util.sanitize_fn(semester_programme) + '.md'), 'w', encoding='utf-8') as f:
+                f.write(programme_md_heading + "\n\n")
+                f.write(programme_md_heading_index + "\n\n")
+                f.write(programme_md_tables)
+                f.write(programme_md_footer)
+
+    def _make_md_table(self, study_programme: str, course_semester_data: List[Dict[str, Union[str, int, float]]]):
+        """ Make markdown table from data of a single semester-course. """
+        if not course_semester_data:
+            return ""
+
+        data_item = course_semester_data[0]
+        course_id = data_item['course_id']
+        course_name = data_item['course_name']
+        enrolled_total = data_item['enrolled']
+
+        row_headers = '|                          |'
+        row_separator = '|--------------------------|'
+        row_completed_total = "|**Splněno celkem**        |"
+        row_completed_total_percent = "|**Splněno celkem procent**|"
+
+        row_data_headers = []
+        row_data_separator = []
+        row_data_compl_total = []
+        row_data_compl_total_percent = []
+
+        previous_completed = 0
+        previous_completed_percent = 0
+
+        for datapoint in course_semester_data:
+            row_data_headers.append(util.timestamp_to_date_str(datapoint['timestamp']))
+            row_data_separator.append("-" * 20)
+
+            new_completed = datapoint['finished']
+            new_completed_percent = datapoint['percent_finished']
+            completed_delta = new_completed - previous_completed
+            completed_percent_delta = new_completed_percent - previous_completed_percent
+            previous_completed = new_completed
+            previous_completed_percent = new_completed_percent
+
+            row_data_compl_total.append(f'''{new_completed} ({completed_delta:+})''')
+            row_data_compl_total_percent.append(
+                f'''{datapoint['percent_finished']*100:.0f}% ({completed_percent_delta*100:+.0f}%)''')
+
+        md = f"""## {course_name} ({course_id})
+
+**Přihlášeno studentů**: {enrolled_total}
+
+{row_headers}{"|".join(row_data_headers)}|
+{row_separator}{"|".join(row_data_separator)}|
+{row_completed_total}{"|".join(row_data_compl_total)}|
+{row_completed_total_percent}{"|".join(row_data_compl_total_percent)}|
+"""
+
+        return md
+
+
 def parse_page(page: str) -> Dict:
     result = {}
     page = page.decode('utf-8')
@@ -98,62 +245,6 @@ def combine_courses(courses, fn='courses.json'):
     return data
 
 
-def make_md_table(semester: str, study_programme: str, course_semester_data: List[Dict[str, Union[str, int, float]]]):
-    """ Make markdown table from data of a single semester-course. """
-    if not course_semester_data:
-        return ""
-
-    data_item = course_semester_data[0]
-    course_id = data_item['course_id']
-    course_name = data_item['course_name']
-    enrolled_total = data_item['enrolled']
-
-    row_headers = '|                          |'
-    row_separator = '|--------------------------|'
-    row_completed_total = "|**Splněno celkem**        |"
-    row_completed_total_percent = "|**Splněno celkem procent**|"
-
-    row_data_headers = []
-    row_data_separator = []
-    row_data_compl_total = []
-    row_data_compl_total_percent = []
-
-    previous_completed = 0
-    previous_completed_percent = 0
-
-    for datapoint in course_semester_data:
-        row_data_headers.append(util.timestamp_to_date_str(datapoint['timestamp']))
-        row_data_separator.append("-" * 20)
-
-        new_completed = datapoint['finished']
-        new_completed_percent = datapoint['percent_finished']
-        completed_delta = new_completed - previous_completed
-        completed_percent_delta = new_completed_percent - previous_completed_percent
-        previous_completed = new_completed
-        previous_completed_percent = new_completed_percent
-
-        row_data_compl_total.append(f'''{new_completed} ({completed_delta:+})''')
-        row_data_compl_total_percent.append(
-            f'''{datapoint['percent_finished']*100:.0f}% ({completed_percent_delta*100:+.0f}%)''')
-
-    md = f"""## {course_name} ({course_id})
-
-**Přihlášeno studentů**: {enrolled_total}
-
-{row_headers}{"|".join(row_data_headers)}|
-{row_separator}{"|".join(row_data_separator)}|
-{row_completed_total}{"|".join(row_data_compl_total)}|
-{row_completed_total_percent}{"|".join(row_data_compl_total_percent)}|
-"""
-
-    # tgt_dir = os.path.join('page', semester, study_programme)
-    # os.makedirs(tgt_dir, exist_ok=True)
-    # with open(os.path.join(tgt_dir, util.sanitize_fn(course_id) + '.md'), 'w', encoding='utf-8') as f:
-    #     f.write(md)
-
-    return md
-
-
 def load_semester(semester: str):
     fn = f'data/{semester.upper()}.json'
     if not os.path.exists(fn):
@@ -204,93 +295,6 @@ def add_new_course_data(new_data, original_data, timestamp):
         merge_single_course(course_data, original_data, timestamp)
 
 
-def make_index(index_root, courses_root):
-    if not os.path.exists(courses_root):
-        raise ValueError("Specified root directory does not exist.")
-
-    index_md = "# Přehled splněných předmětů\n"
-    programme_semesters_dict = {}
-    for semester in os.listdir(courses_root):
-        for programme in os.listdir(os.path.join(courses_root, semester)):
-            # print(f"{semester} -> {programme}")
-            if programme not in programme_semesters_dict:
-                programme_semesters_dict[programme] = []
-
-            programme_semesters_dict[programme].append(semester)
-
-    for programme, semesters in programme_semesters_dict.items():
-        programme_name_no_ext = programme.rsplit(".", 1)[0]
-
-        index_md += f'## Program {programme_name_no_ext}\n'
-        for semester in semesters:
-            index_md += f'- [{util.semester_id_to_str(semester)}]({os.path.join(os.path.relpath(courses_root, index_root),semester,programme_name_no_ext)})\n'
-
-        index_md += '\n\n'
-
-    with open(os.path.join(index_root, 'README.md'), 'w', encoding='utf-8') as f:
-        f.write(index_md)
-
-
-def make_page_heading_index(index_columns, courses_data):
-    """Create index of anchor links to given courses."""
-    # {course_name} ({course_id})
-
-    index_matrix = []
-    index_rows = math.ceil(len(courses_data) / index_columns)
-    [index_matrix.append([]) for _ in range(index_rows)]
-
-    sorted_courses_data = sorted(courses_data, key=lambda item: item[0]['course_id'])
-    for i, course_data in enumerate(sorted_courses_data):
-        course_id = course_data[0]['course_id']
-        course_name = course_data[0]['course_name']
-        print(course_id)
-        anchor_link = f'#{course_name.lower()}-{course_id.lower()}'.replace(' ', '-').replace('.', '')
-        anchor_full = f'[{course_id}]({anchor_link})'
-
-        cur_row = i // index_columns
-
-        index_matrix[cur_row].append(anchor_full)
-
-    from pprint import pprint
-    pprint(index_matrix)
-    md_heading = f'{"| "*index_columns}|'
-    md_separator = md_heading.replace(' ', '-')
-    md_content = ''
-
-    for row in index_matrix:
-        md_content += f'|{" | ".join(row)}|\n'
-
-    md_index = f'''
-{md_heading}
-{md_separator}
-{md_content}
-    '''
-    print(md_index)
-    return md_index
-
-
-def make_pages(data, semester, root='page', index_columns=6):
-    for semester_programme, semester_courses in data.items():
-        programme_md_heading = f"# {util.semester_id_to_str(semester)} - předměty programu {semester_programme}"
-        programme_md_tables = ""
-        programme_md_footer = f'\n\n*Stav k {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}*'
-
-        programme_md_heading_index = make_page_heading_index(index_columns, semester_courses.values())
-
-        for course_id, course_data in semester_courses.items():
-            md_page = make_md_table(semester, semester_programme, course_data)
-            programme_md_tables += md_page
-            programme_md_tables += "\n"
-
-        tgt_dir = os.path.join(root, semester)
-        os.makedirs(tgt_dir, exist_ok=True)
-        with open(os.path.join(tgt_dir, util.sanitize_fn(semester_programme) + '.md'), 'w', encoding='utf-8') as f:
-            f.write(programme_md_heading + "\n\n")
-            f.write(programme_md_heading_index + "\n\n")
-            f.write(programme_md_tables)
-            f.write(programme_md_footer)
-
-
 def main():
     now = datetime.datetime.now()
     semester = util.get_semester(now)
@@ -302,10 +306,10 @@ def main():
     fn = f'data/{semester.upper()}.json'
     save_data(fn, old_data)
 
-    make_pages(old_data, semester, 'page/courses')
-    make_index('page', 'page/courses')
+    generator = SiteGenerator('page', 'page/courses', old_data, semester)
+    generator.generate_page()
 
-    publisher.publish('page')
+    # publisher.publish('page')
 
 
 if __name__ == '__main__':

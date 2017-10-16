a0daf667db291d5e71e472e97cfb086ed4e153e1
Martin Melka <melkamar@fit.cvut.cz>
2017-10-16 16:22:19 +0200

    Fix parsing semester from date.
    Survey does not get updated with new semester data when semester starts.
    Therefore data until december will be considered to belong to previous
    semester. Hopefully this will fix the issue.

--- util.py
+++ util.py
@@ -16,9 +16,9 @@ def get_semester(date: datetime.datetime) -> str:
 
     if month in [1, 2]:
         return f"B{year-1}1"
-    elif month in [3, 4, 5, 6, 7, 8, 9]:
+    elif month in [3, 4, 5, 6, 7, 8, 9, 10, 11]:
         return f"B{year-1}2"
-    elif month in [10, 11, 12]:
+    elif month in [12]:
         return f"B{year}1"
 
 
